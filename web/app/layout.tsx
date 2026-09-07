4c7de681fd5ebf2ec4554f3cdb8c94cc1866053d
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2026-09-07 11:22:50 -0400

    Apply light ratings mockup theme

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -5,6 +5,7 @@ import "@/styles/ratings.css";
 import "@/styles/advanced.css";
 import "@/styles/team.css";
 import "@/styles/mobile-tables.css";
+import "@/styles/mockup-light.css";
 import "./additions.css";
 
 export const metadata: Metadata = {
@@ -17,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="theme-color" content="#172438" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         {/* eslint-disable-next-line @next/next/no-page-custom-font -- this is the
