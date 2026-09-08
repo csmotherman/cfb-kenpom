2ff6821a30eca39e3b5f8f187435612bd6ecdf2a
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2026-09-08 17:38:18 -0400

    Load matchup redesign styles

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -17,6 +17,7 @@ import "@/styles/team-logo-align.css";
 import "@/styles/auth.css";
 import "@/styles/billing.css";
 import "@/styles/this-week.css";
+import "@/styles/matchup-v2.css";
 import "@/styles/team-profile-public.css";
 import "@/styles/methodology.css";
 import "@/styles/upgrade.css";
