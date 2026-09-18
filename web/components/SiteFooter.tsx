21c706ab9ba694963fbab66dd244375fc6fa7a1d
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2026-09-18 14:39:58 -0400

    Rebrand footer to PRIME

--- web/components/SiteFooter.tsx
+++ web/components/SiteFooter.tsx
@@ -2,30 +2,30 @@ import Link from "next/link";
 
 export default function SiteFooter({ note }: { note: string }) {
   const brandedNote = note
-    .replaceAll("CollegeFootballFocus", "LEILA Ratings")
-    .replaceAll("College Football Focus", "LEILA Ratings")
-    .replaceAll("ARA Advanced Analytics", "LEILA Pro")
-    .replaceAll("Advanced CFF", "LEILA Pro")
-    .replaceAll("Advanced ARA", "LEILA Pro")
+    .replaceAll("CollegeFootballFocus", "PRIME Football")
+    .replaceAll("College Football Focus", "PRIME Football")
+    .replaceAll("ARA Advanced Analytics", "PRIME Advanced")
+    .replaceAll("Advanced CFF", "PRIME Advanced")
+    .replaceAll("Advanced ARA", "PRIME Advanced")
     .replaceAll("AdjNet", "Adj. Net")
     .replaceAll("AdjEM", "Adj. Net")
     .replaceAll("AdjOff", "Adj. Off")
     .replaceAll(/AdjO(?!ff)/g, "Adj. Off")
     .replaceAll("AdjDef", "Adj. Def")
     .replaceAll(/AdjD(?!ef)/g, "Adj. Def")
-    .replaceAll("CFF", "LEILA Ratings");
+    .replaceAll("CFF", "PRIME Football");
 
   return (
     <footer className="site-footer">
       <div className="container site-footer__inner">
         <div className="site-footer__copy">
-          <strong className="site-footer__brand">LEILA Ratings</strong>
+          <strong className="site-footer__brand">PRIME Football</strong>
           <p>{brandedNote}</p>
         </div>
         <div className="site-footer__links">
           <Link href="/methodology">Methodology</Link>
           <Link href="/predictions">Predictions</Link>
-          <Link href="/upgrade">LEILA Pro</Link>
+          <Link href="/upgrade">PRIME Advanced</Link>
         </div>
       </div>
     </footer>
